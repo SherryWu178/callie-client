4c85f8ce3613b7663affe3e78495a963d41ef766
Sherry <e0425530@u.nus.edu>
2020-07-24 00:24:20 +0800

    for now

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000" //API URL (Local)
-// export const BASE_URL = "https://nuscallie-api.herokuapp.com" // API URL (Heroku)
+// export const BASE_URL = "http://localhost:3000" //API URL (Local)
+export const BASE_URL = "https://nuscallie-api.herokuapp.com" // API URL (Heroku)
